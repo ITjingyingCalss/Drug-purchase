a14e751c1bd4571f542ba7769ace6e0f7e04a8b0
Administrator <jiangchangming>
2020-06-16 17:57:18 +0800

    0616jcm

--- src/main/java/com/ygjy/purchasingmanagement1/controller/PurchaseOrderController1.java
+++ src/main/java/com/ygjy/purchasingmanagement1/controller/PurchaseOrderController1.java
@@ -3,10 +3,17 @@ package com.ygjy.purchasingmanagement1.controller;
 import com.ygjy.purchasingmanagement1.pojo.PurchaseOrder;
 import com.ygjy.purchasingmanagement1.service.impl.PurchaseOrderImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -37,10 +44,30 @@ public class PurchaseOrderController1 {
     }
 
     @ResponseBody
+    @RequestMapping("addPurchaseOrder")
+    public int addPurchaseOrder(PurchaseOrder purchaseOrder){
+        return purchaseOrderImpl.addPurchaseOrder(purchaseOrder);
+    }
+
+    @ResponseBody
     @RequestMapping("deletePurchaseOrder")
     public int deletePurchaseOrder(Integer id){
         int i = purchaseOrderImpl.deletePurchaseOrder(id);
         return i;
+    }
+
+    @ResponseBody
+    @RequestMapping("selectLikePurchaseOrder")
+    public List<PurchaseOrder> selectLikePurchaseOrder(String purchaseOrderNumber, String nameOfPurchaseOrder, Integer purchaseState, Date submissionTime, Integer hospitalId){
+        List<PurchaseOrder> purchaseOrders = purchaseOrderImpl.selectLikePurchaseOrder(purchaseOrderNumber, nameOfPurchaseOrder, purchaseState, submissionTime, hospitalId);
+        return purchaseOrders;
+    }
+    @InitBinder
+    public void initBinder(WebDataBinder binder, WebRequest request) {
+
+        //转换日期
+        DateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));// CustomDateEditor为自定义日期编辑器
     }
 
 
